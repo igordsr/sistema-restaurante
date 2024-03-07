c239fc577d805e94fdfaa07bf2fdbbe05cc5f95e
Igor <37842175+igordsr@users.noreply.github.com>
2024-03-07 19:35:53 -0300

    Init Project

---
+++ src/main/java/com/br/sistemarestaurante/SistemaRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.br.sistemarestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRestauranteApplication.class, args);
+	}
+
+}
