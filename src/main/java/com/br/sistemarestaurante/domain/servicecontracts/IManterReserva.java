484ded5e88c00cf6207db547b0e45c17e8f0b918
Lays Vale <laysnogueira96@gmail.com>
2024-03-15 13:00:53 -0300

    buscar reserva por resturante

--- src/main/java/com/br/sistemarestaurante/domain/servicecontracts/IManterReserva.java
+++ src/main/java/com/br/sistemarestaurante/domain/servicecontracts/IManterReserva.java
@@ -1,9 +1,10 @@
 package com.br.sistemarestaurante.domain.servicecontracts;
 
 import com.br.sistemarestaurante.domain.servicecontracts.businessrule.IAlterarStatusReserva;
+import com.br.sistemarestaurante.domain.servicecontracts.businessrule.IBuscarReserva;
 import com.br.sistemarestaurante.domain.servicecontracts.businessrule.IListarReservasRestaurantePeloStatusReserva;
 import com.br.sistemarestaurante.domain.servicecontracts.businessrule.IRegistrarReserva;
 
-public interface IManterReserva extends IRegistrarReserva, IListarReservasRestaurantePeloStatusReserva, IAlterarStatusReserva {
+public interface IManterReserva extends IRegistrarReserva, IListarReservasRestaurantePeloStatusReserva, IAlterarStatusReserva, IBuscarReserva {
 
 }
