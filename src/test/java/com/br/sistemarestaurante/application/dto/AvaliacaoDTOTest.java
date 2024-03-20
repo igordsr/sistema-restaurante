17421387c7941c7f96f0fc9140469879abc1fa25
Lays Vale <laysnogueira96@gmail.com>
2024-03-19 21:01:51 -0300

    dtos unit test

---
+++ src/test/java/com/br/sistemarestaurante/application/dto/AvaliacaoDTOTest.java
@@ -0,0 +1,29 @@
+package com.br.sistemarestaurante.application.dto;
+
+import com.br.sistemarestaurante.domain.entity.Avaliacao;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AvaliacaoDTOTest {
+    private int classificacao;
+
+    @Test
+    void testToDomainEntity() {
+        UUID id = UUID.randomUUID();
+        UUID reservaId = UUID.randomUUID();
+        String comentario = "Excelente serviço!";
+        int classificacao = 5;
+
+        AvaliacaoDTO avaliacaoDTO = new AvaliacaoDTO(id, reservaId, comentario, classificacao);
+
+        Avaliacao avaliacao = avaliacaoDTO.ToDomainEntity();
+
+        assertEquals(id, avaliacao.getIdentificador());
+        assertEquals(reservaId, avaliacao.getReservaId());
+        assertEquals(comentario, avaliacao.getComentario());
+        assertEquals(classificacao, avaliacao.getClassificacao());
+    }
+}
