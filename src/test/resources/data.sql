00f28566dc487e477d006b7dcf266f65b9e023f4
Lays Vale <laysnogueira96@gmail.com>
2024-03-21 23:21:16 -0300

    config integratio test

---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes (ID, NOME, EMAIL, TELEFONE)
+VALUES
+    ('8508a4c4-d19a-49bc-81b0-8131548b691e', 'NOME TESTE', 'teste@teste.com', '11999999999'),
+    ('42b3b961-b5a5-4448-bbb9-73902b79c8c9', 'NOME TESTE 2', 'teste2@teste.com', '11988888888'),
+    ('10fe49e6-aaaf-43e7-8e93-2b62bd0ec1d9', 'NOME TESTE 3', 'teste3@teste.com', '11977777777');
