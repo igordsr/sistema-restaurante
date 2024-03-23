0a357b176fd35f012854ab270220c1c045c8866c
Elvis <elvisjulius@hotmail.com>
2024-03-23 10:09:04 -0300

    merge

--- src/main/java/com/br/sistemarestaurante/infrastructure/persistence/service/AvaliacaoService.java
+++ src/main/java/com/br/sistemarestaurante/infrastructure/persistence/service/AvaliacaoService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class AvaliacaoService implements IAvaliacaoRepositoryDomainContract {
