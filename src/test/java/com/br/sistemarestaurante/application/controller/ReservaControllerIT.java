1f022b070d79e82433744f923a7c897ff3e8c9d5
Lays Vale <laysnogueira96@gmail.com>
2024-03-22 23:02:22 -0300

    insert profile test

--- src/test/java/com/br/sistemarestaurante/application/controller/ReservaControllerIT.java
+++ src/test/java/com/br/sistemarestaurante/application/controller/ReservaControllerIT.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalTime;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ReservaController.class)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class ReservaControllerIT {
     @Autowired
--- src/test/java/com/br/sistemarestaurante/infrastructure/persistence/repository/IClienteRepositoryIT.java
+++ src/test/java/com/br/sistemarestaurante/infrastructure/persistence/repository/IClienteRepositoryIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -13,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
+@ActiveProfiles("test")
 @Transactional
 class IClienteRepositoryIT {
 
