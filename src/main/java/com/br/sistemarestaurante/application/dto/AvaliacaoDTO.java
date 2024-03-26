f4a876de571c0fab75804c6f550e51412e8dc219
Igor <37842175+igordsr@users.noreply.github.com>
2024-03-26 20:38:36 -0300

    Implemnetação do swagger e documentação

--- src/main/java/com/br/sistemarestaurante/application/dto/AvaliacaoDTO.java
+++ src/main/java/com/br/sistemarestaurante/application/dto/AvaliacaoDTO.java
@@ -3,6 +3,7 @@ package com.br.sistemarestaurante.application.dto;
 import com.br.sistemarestaurante.application.util.IConverterToDomainEntity;
 import com.br.sistemarestaurante.domain.entity.Avaliacao;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
@@ -12,9 +13,11 @@ public record AvaliacaoDTO(
         UUID id,
         @NotNull(message = "O Atributo [reservaId] da Avaliação não pode estar em nulo.")
         UUID reservaId,
+        @Schema(example = "Serve ótimos coquetéis · Serve pratos veganos · Bom para assistir a esportes")
         @NotNull(message = "O Atributo [comentario] da Avaliação não pode estar em nulo.")
         @NotBlank(message = "O Atributo [comentario] da Avaliação não pode estar em Branco.")
         String comentario,
+        @Schema(example = "1")
         @NotNull(message = "O Atributo [classificacao] da Avaliação não pode estar em nulo.")
         int classificacao) implements IConverterToDomainEntity<Avaliacao> {
     @Override
--- src/main/java/com/br/sistemarestaurante/application/dto/ReservaDTO.java
+++ src/main/java/com/br/sistemarestaurante/application/dto/ReservaDTO.java
@@ -6,9 +6,11 @@ import com.br.sistemarestaurante.domain.entity.Cliente;
 import com.br.sistemarestaurante.domain.entity.Reserva;
 import com.br.sistemarestaurante.domain.entity.StatusReserva;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalTime;
 import java.util.Calendar;
@@ -19,24 +21,26 @@ public record ReservaDTO(
         UUID id,
         @NotNull(message = "O Atributo [restauranteId] da Reserva não pode estar em nulo.")
         UUID restauranteId,
-
+        @Schema(example = "Isis Sueli Heloise dos Santos")
         @NotNull(message = "O Atributo [nomeCliente] da Reserva não pode estar em nulo.")
         @NotBlank(message = "O Atributo [nomeCliente] da Reserva não pode estar em Branco.")
         String nomeCliente,
 
+        @Schema(example = "isis_dossantos@eximiart.com.br")
         @Email(message = "E-mail inválido")
         @NotNull(message = "O Atributo [emailCliente] da Reserva não pode estar em nulo.")
         @NotBlank(message = "O Atributo [emailCliente] da Reserva não pode estar em Branco.")
         String emailCliente,
-
+        @Schema(example = "(21) 98795-3698")
         @NotNull(message = "O Atributo [telefoneCliente] da Reserva não pode estar em nulo.")
         @NotBlank(message = "O Atributo [telefoneCliente] da Reserva não pode estar em Branco.")
         String telefoneCliente,
         @NotPastDate
         @NotNull(message = "O Atributo [data] da Reserva não pode estar em nulo.")
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
         Calendar data,
-
         @NotNull(message = "O Atributo [hora] da Reserva não pode estar em nulo.")
+        @DateTimeFormat(pattern = "HH:mm")
         LocalTime hora,
         @JsonInclude(JsonInclude.Include.NON_NULL)
         StatusReserva status
